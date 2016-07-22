b175cce2ad7f05fd612c0e1ef74c4981cc2ed71f
Aleksey Ivashin <alekseyivashin806@yandex.ru>
2016-07-22 18:39:56 +1000

    плавающее меню "в начало" в мобильной версии

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Created by aleks on 22.07.2016.
+ */
+var deviceWidth = $(document).width();
+
+if (deviceWidth <= 642) {
+    var headerHeight = $("header .container").height();
+    console.log(headerHeight);
+    var topButton = $("#topButton");
+    $(document).scroll(function () {
+        if ($(document).scrollTop() > headerHeight) {
+            topButton.css({"position": "fixed",
+                "top": "0",
+                "background-color": "#5584b1",
+                "width": "100%",
+                "margin": "0"
+            });
+        } else {
+            topButton.css({"position": "static"});
+        }
+    });
+
+}
